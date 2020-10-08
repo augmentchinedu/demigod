086758d5d73cb4e812b2a7699110aed0b6590e08
augmentchinedu <augmentchinedu@gmail.com>
2020-10-08 06:29:10 +0100

    Vue JS Added Pretty Json Added

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <vue-json-pretty
+      :path="'res'"
+      :data="{ key: 'value' }"
+      @click="handleClick"
+    >
+    </vue-json-pretty>
+  </div>
+</template>
+
+<script>
+  import VueJsonPretty from "vue-json-pretty";
+  import "vue-json-pretty/lib/styles.css";
+
+  export default {
+    components: {
+      VueJsonPretty
+    }
+  };
+</script>
