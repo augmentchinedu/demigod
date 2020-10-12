c1da3a3d8a9cc234dd6eceaf7ff6cd9011003957
augmentchinedu <augmentchinedu@gmail.com>
2020-10-12 11:27:00 +0100

    New Index

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const config = require("./config");
 const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
 const app = express();
-
 app.use(cors());
 app.use(morgan("tiny"));
 
 app.use(express.static(path.join(__dirname, "client", "dist")));
-
-app.listen(config.app.port, () => {
-  console.log(`Server Started @ Port ${config.app.port}`);
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
+
+app.listen(process.env.PORT || 3000);
