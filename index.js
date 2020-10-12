65d99e5408f0d286e852fe56ee23abc37fc55813
augmentchinedu <augmentchinedu@gmail.com>
2020-10-12 12:07:03 +0100

    Try again

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 
+
 const app = express();
 app.use(cors());
 app.use(morgan("tiny"));
 
 app.use(express.static(path.join(__dirname, "client", "dist")));
-app.use("*", (req, res) => {
+app.all("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3001);
